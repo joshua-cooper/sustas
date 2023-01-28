bb9d6765d60447e7a5bf11d14d86b75b5e707de1
Josh Cooper <josh@cooper.dev>
2023-01-28 22:23:42 +0900

    Refactor the wifi module to be more robust
    This refactor better models state transitions and handles errors more
    gracefully.

--- src/dbus.rs
+++ src/dbus.rs
@@ -1,10 +1,13 @@
 //! Utilities for working with dbus.
 
+use futures_util::{future::OptionFuture, stream::Next, StreamExt};
 use std::cmp::PartialEq;
 use zbus::{
-    fdo::ObjectManagerProxy,
-    zvariant::{OwnedObjectPath, Value},
+    fdo::ManagedObjects,
+    zvariant::{ObjectPath, Value},
+    PropertyStream,
 };
+use zbus::{fdo::ObjectManagerProxy, zvariant::OwnedObjectPath};
 
 /// Finds the path of an interface with a given property value.
 ///
@@ -34,4 +37,43 @@ where
     }
 
     Ok(None)
+}
+
+/// Gets the path of an interface with a given property value.
+///
+/// If no property with the given name and value exsts on the given interface, this returns `None`.
+pub fn get_object_path<'a, V>(
+    objects: &'a ManagedObjects,
+    interface_name: &str,
+    property_name: &str,
+    property_value: &V,
+) -> Option<ObjectPath<'a>>
+where
+    V: PartialEq + ?Sized,
+    for<'v> &'v V: TryFrom<&'v Value<'v>>,
+{
+    for (path, interfaces) in objects {
+        let is_match = interfaces
+            .get(interface_name)
+            .and_then(|properties| properties.get(property_name))
+            .and_then(|name| name.downcast_ref::<V>())
+            .map(|name| name == property_value)
+            .unwrap_or_default();
+
+        if is_match {
+            return Some(path.as_ref());
+        }
+    }
+
+    None
+}
+
+/// Returns an [`OptionFuture`] future for the next change in a [`PropertyStream`].
+pub fn option_change<'a, 'b, T>(
+    stream: Option<&'a mut PropertyStream<'b, T>>,
+) -> OptionFuture<Next<'a, PropertyStream<'b, T>>>
+where
+    T: Unpin,
+{
+    stream.map(StreamExt::next).into()
 }
